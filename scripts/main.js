d2182ccc609dcd7117586147e533b6e2ca71b59c
Ingvar4 <jamessoer91@gmail.com>
2024-06-30 20:48:24 +0300

    another fix

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,7 @@ function toggleVideo() {
 function changeBg(bg, title) {
     const banner = document.querySelector('.banner');
     const contents = document.querySelectorAll('.content');
-    banner.style.background = `url("./images/planets/${bg}")`;
+    banner.style.background = `url("images/planets/${bg}")`;
     banner.style.backgroundSize = 'cover';
     banner.style.backgroundPosition = 'center';
 
